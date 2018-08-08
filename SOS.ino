60a58a1ba0a209e814510e835ac7087b5af6d344
YAHYA HASSAN ASIRI <Yahyawii@gmail.com>
2018-08-08 19:00:37 +1000

    Add files via upload

---
+++ SOS.ino
@@ -0,0 +1,55 @@
+
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+ void loop()
+ {
+      
+
+  //signal S
+      digitalWrite(LED_BUILTIN, HIGH);  
+      delay(150);                      
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(100);
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(150);
+      digitalWrite(LED_BUILTIN, LOW);    
+      delay(100); 
+      digitalWrite(LED_BUILTIN, HIGH);  
+      delay(150);                      
+      digitalWrite(LED_BUILTIN, LOW);    
+      delay(100);
+
+  //signal O
+      digitalWrite(LED_BUILTIN, HIGH);  
+      delay(500);                      
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(1000);
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(500);
+      digitalWrite(LED_BUILTIN, LOW);    
+      delay(1000); 
+      digitalWrite(LED_BUILTIN, HIGH);  
+      delay(500);                      
+      digitalWrite(LED_BUILTIN, LOW);    
+      delay(1000);
+     
+  //signal S
+      digitalWrite(LED_BUILTIN, HIGH);  
+      delay(150);                      
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(100);
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(150);
+      digitalWrite(LED_BUILTIN, LOW);    
+      delay(100); 
+      digitalWrite(LED_BUILTIN, HIGH);  
+      delay(150);                      
+      digitalWrite(LED_BUILTIN, LOW);    
+      delay(100);
+
+      delay(2500);
+                                         
+}
